c90552680f560752fd85be01d6d16065d8400632
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-01-16 19:29:17 -0600

    commit tree 39503a7011d6cd9558ab80ab31712324d0e2ed47

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 16;
+let num4 = 6;
 
-// Write your code here
+
+let multiply = num1 * num2
+let random = Math.ceil(Math.random() * 10);
+let mod = num3 % num4
+let max = Math.max(1, 15, 6, 20, 4);
+
+//multiply;
+//random;
+//mod;
+//max;
